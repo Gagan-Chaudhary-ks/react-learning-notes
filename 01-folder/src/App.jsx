92d758d53219cfa3c9d0ff1da7131eb262904ffb
gagan <gaganjio14@gmail.com>
2026-04-19 12:54:18 +0530

    React: 01-folder initial setup

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id='parent'>
+      <h1 id='child1'>hello</h1>
+      <h2 id='child2'>hello</h2>
+    </div>
+    </>
+  )
+}
+
+export default App
